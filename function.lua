7f84399d8c2f55ec7ec20f4d5f033a2d183e2d96
ahin0422 <ahinahn0422@gmail.com>
2026-06-15 22:17:12 +0900

    add

---
+++ function.lua
@@ -0,0 +1,9 @@
+local function hello() 
+    print("hello")
+    return 0
+end
+
+hello()
+print(type(hello()))
+print(hello)
+print(hello())
---
+++ string.lua
@@ -0,0 +1,10 @@
+str = "hello"
+print(string.upper(str))
+
+date = 15
+month = 6
+year = 2026
+print(string.format("Date %02d/%02d/%03d", date, month, year))
+
+print(str.." lua")
+print(string.rep("lua ", 10))
